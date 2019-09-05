02515a61f56176b656323483ef49a9cfa7a9302b
Joel Vitelli <jvitelli92@gmail.com>
2019-09-05 12:50:30 -0300

    Agregar archivos de proyecto.

---
+++ WebForm1.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="LINQ_APP.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <asp:GridView ID="GridView1" runat="server">
+
+            </asp:GridView>
+
+
+            <br />
+            <asp:Button ID="btnGetData" runat="server" OnClick="btnGetData_Click" Text="Get Data" />
+            <asp:Button ID="btnInsert" runat="server" OnClick="btnInsert_Click" Text="Insert" />
+
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,56 @@
+﻿using LINQ_APP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LINQ_APP
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void GetData()
+        {
+            SampleDataContext dbContex = new SampleDataContext();
+            GridView1.DataSource = dbContex.Employees;
+            GridView1.DataBind();
+        }
+
+        protected void btnGetData_Click(object sender, EventArgs e)
+        {
+            GetData();
+        }
+
+        protected void btnInsert_Click(object sender, EventArgs e)
+        {
+
+            using (SampleDataContext dbContex = new SampleDataContext())
+            {
+                Employees newEmployee = new Employees
+                {
+                    FirstName = "Pamela",
+                    LastName = "Vazques",
+                    Title = "Costurera",
+                    TitleOfCourtesy = "Sra.",
+                    Address = "Echeverria 158",
+                    City = "Mendoza",
+                    Region = "Cuyo",
+                    PostalCode = "5598",
+                    Country = "Argentina",
+                    HomePhone = "4369852",
+                    Extension = "SDFW",
+                    Notes = "Nada we"
+                };
+                dbContex.Employees.InsertOnSubmit(newEmployee);
+                dbContex.SubmitChanges();
+            }
+            GetData();
+        }
+    }
+}
